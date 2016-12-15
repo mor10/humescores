f121e467f0f29e986c2fc21bbfb0b9000e3e4481
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 15:08:39 -0800

    10_06

--- functions.php
+++ functions.php
@@ -159,7 +159,7 @@ function humescores_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'humescores' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'humescores' ),
+		'description'   => esc_html__( 'Add sidebar widgets here.', 'humescores' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
