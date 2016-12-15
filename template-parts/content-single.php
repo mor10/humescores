1ee1edc89a234386b9841ba1ef2d442b2b28ed88
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 14:57:17 -0800

    09_03

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -26,6 +26,15 @@
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
+	
+	<?php
+	if ( has_post_thumbnail() ) { ?>
+	<figure class="featured-image full-bleed">
+		<?php
+		the_post_thumbnail();
+		?>
+	</figure><!-- .featured-image full-bleed -->
+	<?php } ?>
 
 	<section class="post-content">
 		
