769c3d77d70e7e32a2dc6a2977fc8fc93935539a
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 15:19:59 -0800

    11_03

--- inc/extras.php
+++ inc/extras.php
@@ -22,6 +22,7 @@ function humescores_body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
+		$classes[] = 'archive-view';
 	}
 	
 	// Add a class telling us if the sidebar is in use.
