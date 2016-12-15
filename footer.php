f9b85b241cabcc36b11d2887c080a4b0af8244c6
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 12:54:56 -0800

    05_06_end

--- footer.php
+++ footer.php
@@ -14,6 +14,11 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		
+		<nav class="social-menu">
+		<?php wp_nav_menu( array( 'theme_location' => 'social' ) ); ?>
+		</nav><!-- .social-menu -->
+		
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humescores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'humescores' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ function humescores_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Header', 'humescores' ),
+		'social' => esc_html__( 'Social Media Menu', 'humescores' ),
 	) );
 
 	/*
