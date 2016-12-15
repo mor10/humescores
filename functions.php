184c9eb47d4ed58e0f74b961cdd3484f05af711f
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 12:01:47 -0800

    03_05

--- functions.php
+++ functions.php
@@ -68,6 +68,45 @@ function humescores_setup() {
 endif;
 add_action( 'after_setup_theme', 'humescores_setup' );
 
+
+/**
+ * Register custom fonts.
+ */
+function humescores_fonts_url() {
+	$fonts_url = '';
+
+	/**
+	 * Translators: If there are characters in your language that are not
+	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'humescores' );
+	$pt_serif = _x( 'on', 'PT Serif font: on or off', 'humescores' );
+
+	$font_families = array();
+	
+	if ( 'off' !== $source_sans_pro ) {
+		$font_families[] = 'Source Sans Pro:400,400i,700,900';
+	}
+	
+	if ( 'off' !== $pt_serif ) {
+		$font_families[] = 'PT Serif:400,400i,700,700i';
+	}
+	
+	
+	if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -103,7 +142,7 @@ add_action( 'widgets_init', 'humescores_widgets_init' );
  */
 function humescores_scripts() {
 	// Enqueue Google Fonts: Source Sans Pro and PT Serif
-	wp_enqueue_style( 'humescores-fonts', 'https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i|Source+Sans+Pro:400,400i,600,900' );
+	wp_enqueue_style( 'humescores-fonts', humescores_fonts_url() );
 	
 	wp_enqueue_style( 'humescores-style', get_stylesheet_uri() );
 
