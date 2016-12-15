38b5bc47a220af83ac6c0560234dd9ac6cd00a9c
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 15:38:36 -0800

    11_12

--- search.php
+++ search.php
@@ -9,15 +9,20 @@
 
 get_header(); ?>
 
+<?php
+if ( have_posts() ) : ?>
+
+	<header class="page-header">
+		<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'humescores' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+	</header><!-- .page-header -->
+
+<?php endif; ?>
+
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
 		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'humescores' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
@@ -28,11 +33,15 @@ get_header(); ?>
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'template-parts/content' );
 
 			endwhile;
 
-			the_posts_navigation();
+			the_posts_pagination( array(
+				'prev_text' => __( 'Previous', 'humescores' ),
+				'next_text' => __( 'Next', 'humescores' ),
+				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page', 'humescores' ) . '</span>',
+			));
 
 		else :
 
