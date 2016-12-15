15ca1df54dc588761703ba83abd5cf6a226c8fa7
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 12:59:27 -0800

    06_03

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -44,13 +44,8 @@ if ( ! function_exists( 'humescores_entry_footer' ) ) :
  * Prints HTML with meta information for the categories, tags and comments.
  */
 function humescores_entry_footer() {
-	// Hide category and tag text for pages.
+	// Hide tag text for pages.
 	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'humescores' ) );
-		if ( $categories_list && humescores_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'humescores' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
 
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'humescores' ) );
@@ -77,6 +72,18 @@ function humescores_entry_footer() {
 	);
 }
 endif;
+
+/**
+ * Display category list
+ */
+
+function humescores_the_category_list() {
+	/* translators: used between list items, there is a space after the comma */
+	$categories_list = get_the_category_list( esc_html__( ', ', 'humescores' ) );
+	if ( $categories_list && humescores_categorized_blog() ) {
+		printf( '<span class="cat-links">' . esc_html__( '%1$s', 'humescores' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+	}
+}
 
 /**
  * Returns true if a blog has more than 1 category.
--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<?php humescores_the_category_list(); ?>
 		<?php
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
