4c7cf48b5fcc3435df79754909bbf0a670a69cb4
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 13:32:39 -0800

    06_10_end

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -128,3 +128,18 @@ function humescores_category_transient_flusher() {
 }
 add_action( 'edit_category', 'humescores_category_transient_flusher' );
 add_action( 'save_post',     'humescores_category_transient_flusher' );
+
+
+/**
+ * Post navigation (previous / next post) for single posts.
+ */
+function humescores_post_navigation() {
+	the_post_navigation( array(
+		'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'humescores' ) . '</span> ' .
+			'<span class="screen-reader-text">' . __( 'Next post:', 'humescores' ) . '</span> ' .
+			'<span class="post-title">%title</span>',
+		'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'humescores' ) . '</span> ' .
+			'<span class="screen-reader-text">' . __( 'Previous post:', 'humescores' ) . '</span> ' .
+			'<span class="post-title">%title</span>',
+	) );
+}
--- single.php
+++ single.php
@@ -17,7 +17,7 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+			humescores_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
