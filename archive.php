3c4482db2b79a744eefecca40545072f86fa307b
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 15:37:28 -0800

    11_11

--- archive.php
+++ archive.php
@@ -9,18 +9,23 @@
 
 get_header(); ?>
 
+<?php
+if ( have_posts() ) : ?>
+
+	<header class="page-header">
+		<?php
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="archive-description">', '</div>' );
+		?>
+	</header><!-- .page-header -->
+	
+<?php endif; ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
 		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
@@ -35,7 +40,11 @@ get_header(); ?>
 
 			endwhile;
 
-			the_posts_navigation();
+			the_posts_pagination( array(
+				'prev_text' => __( 'Previous', 'humescores' ),
+				'next_text' => __( 'Next', 'humescores' ),
+				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page', 'humescores' ) . '</span>',
+			));
 
 		else :
 
