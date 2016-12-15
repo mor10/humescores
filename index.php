d767241de79816898e3b71875848bba706889274
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 15:47:39 -0800

    fix

--- index.php
+++ index.php
@@ -14,11 +14,12 @@
 
 get_header(); ?>
 
+<?php if ( have_posts() ) : ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) :
 
 			if ( is_home() && ! is_front_page() ) : ?>
 				<header>
@@ -46,11 +47,7 @@ get_header(); ?>
 				'before_page_number' => '<span class="screen-reader-text">' . __( 'Page', 'humescores' ) . '</span>',
 			));
 			
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
@@ -58,3 +55,11 @@ get_header(); ?>
 <?php
 get_sidebar();
 get_footer();
+
+
+else :
+
+	get_template_part( 'template-parts/content', 'none' );
+	return;
+
+endif; 
