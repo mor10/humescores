7a8b5dfd5595d721701803c75f59008863585a48
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 15:23:39 -0800

    11_06

--- template-parts/content.php
+++ template-parts/content.php
@@ -30,16 +30,7 @@
 
 		<div class="entry-content">
 			<?php
-				the_content( sprintf(
-					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'humescores' ), array( 'span' => array( 'class' => array() ) ) ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) );
-
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'humescores' ),
-					'after'  => '</div>',
-				) );
+				the_excerpt();
 			?>
 		</div><!-- .entry-content -->
 		
