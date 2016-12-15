da46fb864cef0557480ff238603f986b3fe16a46
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 12:12:35 -0800

    04_04

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'humescores' ); ?></a>
 
-	<?php if ( get_header_image() ) : ?>
+	<?php if ( get_header_image() && is_front_page() ) : ?>
 	<figure class="header-image">
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 			<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
