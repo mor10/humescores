200116f0ffa6c727617daa55ae73aa58e46cc9c4
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-15 12:17:06 -0800

    05_02

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ function humescores_setup() {
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'humescores' ),
+		'primary' => esc_html__( 'Header', 'humescores' ),
 	) );
 
 	/*
