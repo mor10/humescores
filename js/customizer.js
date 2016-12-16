476f070a600187d84d1258f46da3fab28e0f8053
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-16 09:54:15 -0800

    14_04

--- js/customizer.js
+++ js/customizer.js
@@ -39,4 +39,13 @@
 			}
 		} );
 	} );
+
+	// Background color for header and footer.
+	wp.customize( 'theme_bg_color', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-header, .site-footer' ).css( {
+				'background-color': to
+			} );
+		} );
+	} );
 } )( jQuery );
