092321bac0671dbfd1c878541bd0a2c5196e9ddd
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-21 10:40:21 -0800

    Conditional footer menu

--- footer.php
+++ footer.php
@@ -12,22 +12,18 @@
 ?>
 
 	</div><!-- #content -->
-	
+
 	<?php get_sidebar( 'footer' ); ?>
-	
+
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-footer__wrap">
+			<?php
+			// Make sure there is a social menu to display.
+			if ( has_nav_menu( 'social' ) ) { ?>
 			<nav class="social-menu">
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'social',
-						'menu_class'     => 'social-links-menu',
-						'depth'          => 1,
-						'link_before'    => '<span class="screen-reader-text">',
-						'link_after'     => '</span>' . humescores_get_svg( array( 'icon' => 'chain' ) ),
-					) );
-				?>
+				<?php wp_nav_menu( array( 'theme_location' => 'social' ) ); ?>
 			</nav><!-- .social-menu -->
+			<?php } ?>
 
 			<div class="site-info">
 				<div><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humescores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'humescores' ), 'WordPress' ); ?></a></div>
